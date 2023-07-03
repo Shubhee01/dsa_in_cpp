cbbf83f517b77da3ebc8ee506519bf715a9a6ab6
Shubhi Choubey <77564393+Shubhee01@users.noreply.github.com>
2023-07-03 18:53:37 +0530

    Time: 2680 ms (5.75%), Space: 421.8 MB (14.32%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
-    int f(int i, int j, string &s1, string &s2, vector<vector<int>> &dp){
-        if(j<0){
+    int f(int i, int j, string s, string t, vector<vector<int>> &dp){
+        if(j==t.length()){
             return 1;
         }
-        if(i<0){
+        if(i==s.length()){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(s1[i]==s2[j]){
-            return dp[i][j]=f(i-1,j-1,s1,s2,dp)+f(i-1,j,s1,s2,dp);
+        if(s[i]==t[j]){
+            return dp[i][j]=f(i+1,j+1,s,t,dp)+f(i+1,j,s,t,dp);
         }
-        return dp[i][j]=f(i-1,j,s1,s2,dp);
-    }
-    int mod=1e9+7;
-    int f2(int i, int j, string &s1, string &s2){
-        vector<vector<int>> dp(i+1,vector<int>(j+1,0));
-        for(int a=0;a<=i;a++){
-            dp[a][0]=1;
-        }
-        for(int a=1;a<=i;a++){
-            for(int b=1;b<=j;b++){
-                if(s1[a-1]==s2[b-1]){
-                    dp[a][b]=(dp[a-1][b-1]+dp[a-1][b])%mod;
-                }else{
-                    dp[a][b]=dp[a-1][b]%mod;
-                }
-            }
-        }
-        return dp[i][j];
+        return dp[i][j]=f(i+1,j,s,t,dp);
     }
     int numDistinct(string s, string t) {
         int n=s.length();
         int m=t.length();
-        // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        // return f(n-1,m-1,s,t,dp);
-        return f2(n,m,s,t);
-        
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        return f(0,0,s,t,dp);
     }
 };
