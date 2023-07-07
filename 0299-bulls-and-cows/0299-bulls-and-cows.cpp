182efe7dfae929337e56c66f067a987228e358da
Shubhi Choubey <77564393+Shubhee01@users.noreply.github.com>
2023-07-08 02:57:45 +0530

    Time: 7 ms (39.06%), Space: 6.7 MB (12.73%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -5,31 +5,23 @@ public:
         for(auto i:secret){
             m1[i]++;
         }
-        map<int,int> m2;
-        for(auto i:guess){
-            m2[i]++;
-        }
         int n=secret.length();
         int b=0;
         int c=0;
         for(int i=0;i<n;i++){
             if(secret[i]==guess[i]){
-                // cout<<i<<" "<<secret[i]<<endl;
                 b++;
                 m1[secret[i]]--;
-                m2[secret[i]]--;
             }
         }
         for(int i=0;i<n;i++){
             if(secret[i]!=guess[i]){
                 if(m1.find(guess[i])!=m1.end() && m1[guess[i]]>0){
-                    // cout<<m1[guess[i]]<<" "<<guess[i]<<endl;
                     c++;
                     m1[guess[i]]--;
                 }
             }
         }
-        // cout<<b<<" "<<c<<endl;
         string ans="";
         ans+=to_string(b);
         ans+="A";
