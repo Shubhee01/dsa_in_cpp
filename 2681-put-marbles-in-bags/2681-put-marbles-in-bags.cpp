770ff8195f9f34382b87046fa59000341b99d5fc
Shubhi Choubey <77564393+Shubhee01@users.noreply.github.com>
2023-07-09 11:44:44 +0530

    Time: 222 ms (43.64%), Space: 69 MB (43.77%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
-priority_queue<int> pq1;
-    priority_queue<int, vector<int>, greater<int>> pq2;k--;
-   for (int i = 0; i < weights.size() - 1; i++) {
-    pq1.push(weights[i] + weights[i + 1]);
-    pq2.push(weights[i] + weights[i + 1]);
-    if (pq1.size() > k ) {
-        pq1.pop();
-        pq2.pop();
-    }
-    }
-   long long ans1 = 0, ans2 = 0;
-   while (!pq1.empty()) {
-    ans1 += pq1.top();
-    ans2 += pq2.top();
-    pq1.pop();
-    pq2.pop();
-   }
-    return ans2 - ans1;
+        vector<long long> v;
+        k--;
+        int n=weights.size();
+        for(int i=0;i<n-1;i++){
+            v.push_back(weights[i]+weights[i+1]);
+        }
+        sort(v.begin(),v.end());
+        long long a=0;
+        long long b=0;
+        n=v.size();
+        for(int i=0;i<k;i++){
+            a+=v[i];
+            b+=v[n-i-1];
+        }
+        return b-a;
     }
 };
